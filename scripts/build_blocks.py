eae61e906e60e07096705b5dd05c626b64591115
ym-xu <yiming.tsui@gmail.com>
2025-09-05 10:23:37 +0200

    sync

--- scripts/build_blocks.py
+++ scripts/build_blocks.py
@@ -7,7 +7,7 @@ import hydra
 @hydra.main(config_path="../config", config_name="mmlb", version_base="1.2")
 def main(cfg):
     dataset = BaseDataset(cfg.dataset)
-    dataset.build_block_tree(cfg.block_tree)
+    dataset.build_block_tree(cfg.dataset.block_tree)
 
 if __name__ == "__main__":
     main()
